8631090ef194eeb15c841f0b73109e43d63e3c52
mohamed samir <mohamed_elshirbiny@hotmail.com>
2024-11-08 00:19:21 +0200

    task-5

---
+++ 0x00-personal_data/encrypt_password.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+"""PAssword encryption module."""
+import bcrypt
+
+
+def hash_password(password: str) -> bytes:
+    """Expects one string argument name password and returns
+    a salted, hashed password, which is a byte string"""
+    return bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
