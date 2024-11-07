7bce87edd9301ee390f36f81bc9a33507a7c5cdb
mohamed samir <mohamed_elshirbiny@hotmail.com>
2024-11-08 00:04:51 +0200

    task-3 again 2

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -42,7 +42,7 @@ def get_logger() -> logging.Logger:
 
 
 def get_db() -> mysql.connector.connection.MySQLConnection:
-    """Connects to the secure MySQL database. """
+    """Connects to the secure MySQL database."""
     return mysql.connector.connection.MySQLConnection(
         user=os.getenv("PERSONAL_DATA_DB_USERNAME", "root"),
         password=os.getenv("PERSONAL_DATA_DB_PASSWORD", ""),
