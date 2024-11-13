b6ebd326852697cce142fc23a47a9ae344d421fd
mohamed samir <mohamed_elshirbiny@hotmail.com>
2024-11-13 03:59:38 +0200

    task-8

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -12,6 +12,8 @@ class BasicAuth(Auth):
     """BasicAuth class definition."""
     def extract_base64_authorization_header(
             self, authorization_header: str) -> str:
+        """Extracts the Base64 part of the
+        Authorization header for Basic Auth."""
         if authorization_header is None:
             return None
         if not isinstance(authorization_header, str):
@@ -19,3 +21,18 @@ class BasicAuth(Auth):
         if not authorization_header.startswith("Basic "):
             return None
         return authorization_header[6:]
+
+    def decode_base64_authorization_header(
+            self, base64_authorization_header: str) -> str:
+        """ Decodes a base64 authorization header and
+        returns the decoded value as a UTF-8 string """
+        if base64_authorization_header is None:
+            return None
+        if not isinstance(base64_authorization_header, str):
+            return None
+        try:
+            decoded_value = base64.b64decode(
+                base64_authorization_header).decode('utf-8')
+            return decoded_value
+        except (TypeError, base64.binascii.Error):
+            return None
