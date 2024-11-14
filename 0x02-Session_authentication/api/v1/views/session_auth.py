d83e21f40998e8494c673d4b34ff976a1f21ab53
mohamed samir <mohamed_elshirbiny@hotmail.com>
2024-11-15 01:43:40 +0200

    task-8

--- 0x02-Session_authentication/api/v1/views/session_auth.py
+++ 0x02-Session_authentication/api/v1/views/session_auth.py
@@ -29,3 +29,13 @@ def login():
     user_json = jsonify(user.to_json())
     user_json.set_cookie(getenv('SESSION_NAME'), id)
     return user_json
+
+
+@app_views.route('/auth_session/logout', methods=['DELETE'],
+                 strict_slashes=False)
+def logout():
+    """Logout session destruction."""
+    from api.v1.app import auth
+    if not auth.destroy_session(request):
+        abort(404)
+    return jsonify({}), 200
