69ce75143696d9f7d984f9f508f2ec6e938ec8b5
mohamed samir <mohamed_elshirbiny@hotmail.com>
2024-11-22 01:11:58 +0200

    task-17

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -67,5 +67,16 @@ def profile():
     return jsonify({"email": user.email}), 200
 
 
+@app.route("/reset_password", method["POST"], strict_slashes=False)
+def get_reset_password_token():
+    """Generates a token."""
+    email = request.form.get("email")
+    try:
+        token = AUTH.get_reset_password_token(email)
+        return jsonify({"email": email, "reset_token": token}), 200
+    except ValueError:
+        abort(403)
+
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port="5000")
