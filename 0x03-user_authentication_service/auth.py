2cd24e94f12efa84f2c3221a17c7ab152f464470
mohamed samir <mohamed_elshirbiny@hotmail.com>
2024-11-21 22:59:52 +0200

    task-10

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -46,3 +46,13 @@ class Auth:
             return checkpw(password.encode(), user.hashed_password)
         except Exception:
             return False
+
+    def create_session(self, email: str) -> str:
+        """Creates a user's session id and saves it."""
+        try:
+            user = self._db.find_user_by(email=email)
+            session_id = _generate_uuid()
+            self._db.update_user(user.id, session_id=session_id)
+            return session_id
+        except Exception:
+            return None
