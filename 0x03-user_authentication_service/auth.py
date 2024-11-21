70e5c82f25aba2bdbe96d4e1eb51d09107ca5ab1
mohamed samir <mohamed_elshirbiny@hotmail.com>
2024-11-21 23:30:05 +0200

    task-12

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -56,3 +56,11 @@ class Auth:
             return session_id
         except Exception:
             return None
+
+    def get_user_from_session_id(self, session_id: str) -> User:
+        """Returns the corresponding user to a session_id."""
+        try:
+            user = self._db.find_user_by(session_id=session_id)
+            return user
+        except Exception:
+            return None
