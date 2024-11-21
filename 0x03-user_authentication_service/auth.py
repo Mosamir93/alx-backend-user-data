0f41cc4e969d2c48eff2907468e9213345146c8d
mohamed samir <mohamed_elshirbiny@hotmail.com>
2024-11-21 22:08:18 +0200

    task-5.1

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -28,7 +28,7 @@ class Auth:
             self._db.find_user_by(email=email)
             raise ValueError("User {} already exists".format(email))
         except NoResultFound:
-            hashed_password = self._hash_password(password)
+            hashed_password = _hash_password(password)
             user = self._db.add_user(email=email,
                                      hashed_password=hashed_password)
             return user
