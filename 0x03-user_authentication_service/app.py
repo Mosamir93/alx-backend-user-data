43f04b6a7746a9fef97d50568f67dccca8514486
mohamed samir <mohamed_elshirbiny@hotmail.com>
2024-11-22 03:20:43 +0200

    19.2

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -79,7 +79,7 @@ def get_reset_password_token():
 
 
 @app.route("/reset_password", methods=["PUT"])
-def update_password() -> str:
+def update_password():
     """Updates the password."""
     email = request.form.get("email")
     reset_token = request.form.get("reset_token")
