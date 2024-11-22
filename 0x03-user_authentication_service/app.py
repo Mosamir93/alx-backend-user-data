307d5ff2e62c5df8177c301f0b63b338c3b4353d
mohamed samir <mohamed_elshirbiny@hotmail.com>
2024-11-22 02:08:40 +0200

    task-19.01

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -90,7 +90,7 @@ def update_password() -> str:
     except ValueError:
         abort(403)
 
-    return jsonify({"email": f"{email}", "message": "Password updated"})
+    return jsonify({"email": f"{email}", "message": "Password updated"}), 200
 
 
 if __name__ == "__main__":
